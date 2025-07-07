3c183bb0132444979da2098a8b3ad0f139130b56
meghanakunja <meghanakunjaists27@gmail.com>
2025-07-07 15:28:24 +0530

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,28 @@
+"""
+Looping statements is known as iterative statements
+these statements are used to run a particular condition no.of times
+they are broadly divided into two types
+1.while
+2.for
+"""
+#whil:it executes when the condition is true
+"""
+syntax: while condition:
+               statements
+               exit condition/incrementation
+"""
+#eg
+i=1 
+while i<=10:
+    print("the value",i)
+    i+=1
+#eg2
+while True:
+    print("the while condition")
+    
+#eg3
+#while False:
+    #print("the while condition")
+#as while is also called entry control
+#loop it just checks the condition in the entrance
+#as default false: false is given as condition it wont execute
